a579091a0b198ed949224c30b38d536f8f8dcb33
Arwe <wandererpony@gmail.com>
2026-06-15 17:15:02 +0700

    Documentation

---
+++ src/base/Main.java
@@ -0,0 +1,17 @@
+package base;
+
+/**
+ * Greeting Project
+ * @author Arthur Fedveresh
+ */
+public class Main {
+    /**
+     * main entry point
+     * @author Arthur Fedveresh
+     * @param args entry point arguments
+     */
+    public static void main(String[] args) {
+
+        System.out.printf("Hello! My name is Arthur\nI am 29 years old \nAnd I like optimised programming languages like Java and C++");
+    }
+}
