348087b9117ae4c408eb3770a2c3145100ec1098
Arwe <wandererpony@gmail.com>
2026-06-15 17:37:57 +0700

    Вывод нескольких строк с парой вычислений

---
+++ src/base/Main.java
@@ -0,0 +1,14 @@
+package base;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Arthur");
+        int calculation = 123 * 456;
+        System.out.println(calculation);
+        System.out.println("*".repeat(10));
+        System.out.print("Alpha " + " " + "Beta" + " " + "Gamma");
+    }
+}
---
+++ src/doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"base","c":"Main","l":"Main()","u":"%3Cinit%3E()"},{"p":"base","c":"Main","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
