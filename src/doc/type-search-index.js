4793889a206c0e974551512f621ef8ef7fe5307b
Arwe <wandererpony@gmail.com>
2026-06-24 15:01:16 +0700

    Код и документация для 2 задачи 2 темы курса

---
+++ src/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"base","l":"Calculator"},{"p":"base","l":"Main"},{"p":"base","l":"Swapper"}];updateSearchResults();
