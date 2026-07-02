6af0e5085ae0412ecdc46759e654c3c6932a50b0
Arwe <wandererpony@gmail.com>
2026-07-02 15:49:54 +0700

    Исправление boolean переменнойй
    Updated temperature conversion outputs to include boiling point check.

--- src/base/Main.java
+++ src/base/Main.java
@@ -7,7 +7,12 @@ package base;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("C2F(0) = " + TemperatureConverter.celsiusToFahrenheit(0));
-        System.out.println("F2C(40) = " + TemperatureConverter.fahrenheitToCelsius(40));
+        boolean isAboveBoiling = false;
+        double temperature = TemperatureConverter.celsiusToFahrenheit(0);
+        isAboveBoiling = (TemperatureConverter.fahrenheitToCelsius(temperature) >= 100);
+        System.out.println("C2F(0) = " + temperature + " (Это выше точки кипения воды? " + isAboveBoiling + ")");
+        temperature = TemperatureConverter.fahrenheitToCelsius(40);
+        isAboveBoiling = (temperature >= 100);
+        System.out.println("F2C(40) = " + temperature + " (Это выше точки кипения воды? " + isAboveBoiling + ")");
     }
 }
