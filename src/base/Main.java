ede658e424243d539d25af5b6f8d741e7fbcd8f0
Arwe <wandererpony@gmail.com>
2026-07-03 02:51:29 +0700

    Замена магического числа на прописанную константу температуры кипения

--- src/base/Main.java
+++ src/base/Main.java
@@ -9,10 +9,10 @@ public class Main {
     public static void main(String[] args) {
         boolean isAboveBoiling = false;
         double temperature = TemperatureConverter.celsiusToFahrenheit(0);
-        isAboveBoiling = (TemperatureConverter.fahrenheitToCelsius(temperature) >= 100);
+        isAboveBoiling = (TemperatureConverter.fahrenheitToCelsius(temperature) >= TemperatureConverter.BOILING_POINT_CELSIUS);
         System.out.println("C2F(0) = " + temperature + " (Это выше точки кипения воды? " + isAboveBoiling + ")");
         temperature = TemperatureConverter.fahrenheitToCelsius(40);
-        isAboveBoiling = (temperature >= 100);
+        isAboveBoiling = (temperature >= TemperatureConverter.BOILING_POINT_CELSIUS);
         System.out.println("F2C(40) = " + temperature + " (Это выше точки кипения воды? " + isAboveBoiling + ")");
     }
 }
