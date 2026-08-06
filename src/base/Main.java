9622cfc237e98e333c897c033f365102d47fba44
Arwe <wandererpony@gmail.com>
2026-08-06 13:58:36 +0700

    Исправление ошибок 06.08.2026
    Исправлены задания 1, 2 и 4:
    1 - Исправлены наименования переменных на camelCase, добавлен конструктор в класс StudentProfile и изменен вывод информации через StringBuilder.ToString
    2 - intMaxValue удалена (Её функцию выполняет встроенная константа Integer.MAX_VALUE). Константам добавлен публичный параметр
    4 - Добавлено хранение последних рассчитанных температур в класс TempConverter, методы проименованы скучным полноценным английским языком во избежание применения сокращений по типу 2 или 4

--- src/base/Main.java
+++ src/base/Main.java
@@ -12,39 +12,42 @@ public class Main {
     static final double CELSIUS_FREEZE_WATER = 0;
     static final double CELSIUS_BODY_TEMPERATURE = 37;
     static final double CELSIUS_CROSS_FAHRENHEIT = -40;
+
     /**
      * Точка входа в программу
      *
      * @author Артур Федьвереш
      */
     public static void main(String[] args) {
-        StudentProfile SP = new StudentProfile();
-        SP.returnStudentInfo();
+        StudentProfile studentProfile = new StudentProfile();
+        studentProfile.setStudentInfo('B', (short) 13, (byte) 2, 1996, 5550505L, 2.3f, 2.21, false);
+        String studentCard = studentProfile.returnStudentInfo();
+        System.out.println(studentCard);
 
         System.out.println("- - - -");
 
-        Overflow OverflowDemonstration = new Overflow();
-        OverflowDemonstration.integerDemonstrations();
+        Overflow overflowDemonstration = new Overflow();
+        overflowDemonstration.integerDemonstrations();
 
         System.out.println("- - - -");
 
-        Finance FinanceDemonstration = new Finance();
-        FinanceDemonstration.inaccurateSumABC(0.1, 0.2, 0);
-        FinanceDemonstration.inaccurateSumABC(10.0, -9.0, -0.9);
-        FinanceDemonstration.sumABC(new BigDecimal("0.1"), new BigDecimal("0.2"), new BigDecimal("0"));
-        FinanceDemonstration.sumABC(new BigDecimal("10.0"), new BigDecimal("-9.0"), new BigDecimal("-0.9"));
+        Finance financeDemonstration = new Finance();
+        financeDemonstration.inaccurateSumABC(0.1, 0.2, 0);
+        financeDemonstration.inaccurateSumABC(10.0, -9.0, -0.9);
+        financeDemonstration.sumABC(new BigDecimal("0.1"), new BigDecimal("0.2"), new BigDecimal("0"));
+        financeDemonstration.sumABC(new BigDecimal("10.0"), new BigDecimal("-9.0"), new BigDecimal("-0.9"));
 
         System.out.println("- - - -");
 
-        TempConverter TC = new TempConverter();
-        TC.convertC2F(CELSIUS_BOILING_WATER);
-        TC.convertC2K(CELSIUS_BOILING_WATER);
-        TC.convertC2F(CELSIUS_FREEZE_WATER);
-        TC.convertC2K(CELSIUS_FREEZE_WATER);
-        TC.convertC2F(CELSIUS_BODY_TEMPERATURE);
-        TC.convertC2K(CELSIUS_BODY_TEMPERATURE);
-        TC.convertC2F(CELSIUS_CROSS_FAHRENHEIT);
-        TC.convertC2K(CELSIUS_CROSS_FAHRENHEIT);
+        TempConverter tempConverter = new TempConverter();
+        tempConverter.convertCelsiusToFahrenheit(CELSIUS_BOILING_WATER);
+        tempConverter.convertCelsiusToKelvin(CELSIUS_BOILING_WATER);
+        tempConverter.convertCelsiusToFahrenheit(CELSIUS_FREEZE_WATER);
+        tempConverter.convertCelsiusToKelvin(CELSIUS_FREEZE_WATER);
+        tempConverter.convertCelsiusToFahrenheit(CELSIUS_BODY_TEMPERATURE);
+        tempConverter.convertCelsiusToKelvin(CELSIUS_BODY_TEMPERATURE);
+        tempConverter.convertCelsiusToFahrenheit(CELSIUS_CROSS_FAHRENHEIT);
+        tempConverter.convertCelsiusToKelvin(CELSIUS_CROSS_FAHRENHEIT);
         System.out.println("Интересный факт: -40 градусов это точка пересечения шкал Цельсия и Фаренгейта, т.е. в этой точке обозначение температур одинаковое");
     }
 
