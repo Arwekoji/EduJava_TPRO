00e06fae4e53bdac231250bf554980be102b9201
Arwe <wandererpony@gmail.com>
2026-08-06 13:59:36 +0700

    Исправление ошибок 06.08.2026
    Исправлены задания 1, 2 и 4:
    1 - Исправлены наименования переменных на camelCase, добавлен конструктор в класс StudentProfile и изменен вывод информации через StringBuilder.ToString
    2 - intMaxValue удалена (Её функцию выполняет встроенная константа Integer.MAX_VALUE). Константам добавлен публичный параметр
    4 - Добавлено хранение последних рассчитанных температур в класс TempConverter, методы проименованы скучным полноценным английским языком во избежание применения сокращений по типу 2 или 4

--- src/base/Overflow.java
+++ src/base/Overflow.java
@@ -6,13 +6,11 @@ package base;
  * @author Артур Федьвереш
  */
 public class Overflow {
-    final short SECONDS_IN_HOUR = 3600;
-    final short HOURS_IN_DAY = 24;
-    final short DAYS_IN_YEAR = 365;
-    final long YEARS_IN_CENTURY = 100L;
-    final long LEAP_DAYS_IN_CENTURY = 25L;
-
-    int intMaxValue = Integer.MAX_VALUE;
+    static final long SECONDS_IN_HOUR = 3600L;
+    static final short HOURS_IN_DAY = 24;
+    static final short DAYS_IN_YEAR = 365;
+    static final short YEARS_IN_CENTURY = 100;
+    static final short LEAP_DAYS_IN_CENTURY = 25;
 
     /**
      * Метод, демонстрирующий на примерах в консоли последствия переполнения (aka выхода за границы) переменной типа int
@@ -21,9 +19,17 @@ public class Overflow {
      */
     public void integerDemonstrations() {
         System.out.println("Переполнение типов:");
-        System.out.println("Результат переполнения int на +1 = " + (intMaxValue + 1)); //Вывод результата переполнения в консоль
+        System.out.println("Максимальное значение типа int = " + Integer.MAX_VALUE);
+        System.out.println("Результат переполнения int на +1 = " + (Integer.MAX_VALUE + 1)); //Вывод результата переполнения в консоль
         long centurySecs = SECONDS_IN_HOUR * HOURS_IN_DAY * (DAYS_IN_YEAR * YEARS_IN_CENTURY + LEAP_DAYS_IN_CENTURY);
         System.out.println("100 лет в секундах как int (Wrong): " + (int) centurySecs);
         System.out.println("100 лет в секундах как long int: " + centurySecs);
+        /*
+            Результат отличается по причине того, что 100 лет в секундах - слишком большое число для типа int.
+            При попытке записать его таким образом произойдет переполнение, из-за которого произойдёт искажение числа.
+            Искаженный результат получается вследствие битовых операций.
+            Чтобы не было переполнения используется тип long, у которого диапазон допустимых значений достаточно большой,
+            чтобы 100 лет в секундах не вызывали переполнение.
+         */
     }
 }
